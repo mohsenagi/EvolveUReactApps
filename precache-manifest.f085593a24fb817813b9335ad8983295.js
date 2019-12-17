4d1ca8dcc83be067a22c1a43ecba9b06382c9e89
Mohsen Aghajani <mohsen.agi@gmail.com>
2019-12-16 21:50:55 -0700

    Updates

---
+++ precache-manifest.f085593a24fb817813b9335ad8983295.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0b71d6f97b56130f8db4ae013ec8c3",
+    "url": "/MyFirstReactApps/index.html"
+  },
+  {
+    "revision": "218949372cd9ce342d02",
+    "url": "/MyFirstReactApps/static/css/main.c9cc41cc.chunk.css"
+  },
+  {
+    "revision": "ba95798f9bf1ce6fe98c",
+    "url": "/MyFirstReactApps/static/js/2.f99701a1.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/MyFirstReactApps/static/js/2.f99701a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "218949372cd9ce342d02",
+    "url": "/MyFirstReactApps/static/js/main.54bd7b10.chunk.js"
+  },
+  {
+    "revision": "08a3166c1accb851c768",
+    "url": "/MyFirstReactApps/static/js/runtime-main.5abb06ad.js"
+  },
+  {
+    "revision": "481f3486b21c8e9c98eb46152613e0b0",
+    "url": "/MyFirstReactApps/static/media/City.481f3486.svg"
+  },
+  {
+    "revision": "166d3514496c372d7b82ec8c9e22e665",
+    "url": "/MyFirstReactApps/static/media/Handshake.166d3514.svg"
+  },
+  {
+    "revision": "05f58590206d66c6241f9478876ec4e3",
+    "url": "/MyFirstReactApps/static/media/LinkedList.05f58590.svg"
+  },
+  {
+    "revision": "04e848f8f484417eea06b36b6874e01b",
+    "url": "/MyFirstReactApps/static/media/Settings.04e848f8.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MyFirstReactApps/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c7091dd08dcf069df8ae00a09f8683d1",
+    "url": "/MyFirstReactApps/static/media/tic tac toe.c7091dd0.svg"
+  }
+]);
