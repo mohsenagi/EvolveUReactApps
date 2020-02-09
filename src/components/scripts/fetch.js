daf1fa3f0cc73a71ba063e5abf9b446cccabf1cf
Mohsen Aghajani <mohsen.agi@gmail.com>
2020-02-09 00:14:05 -0700

    fixing cors

--- src/components/scripts/fetch.js
+++ src/components/scripts/fetch.js
@@ -23,6 +23,26 @@ const fetchfunctions = {
         return json;
     },
 
+    async getData(url = '') {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'GET',     // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',       // no-cors, *cors, same-origin
+            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow',         // manual, *follow, error
+            referrer: 'no-referrer',    // no-referrer, *client
+        });
+        const json = await response.json();    // parses JSON response into native JavaScript objects
+        json.status = response.status;
+        json.statusText = response.statusText;
+        return json;
+    },
+
     async loadLocal () {
         const response = await fetch('https://cors-anywhere.herokuapp.com/https://simplemaps.com/static/data/country-cities/ca/ca.json');
         const json = await response.json();
@@ -30,7 +50,7 @@ const fetchfunctions = {
     },
 
     async load(newCommunity) {
-        let data = await this.postData(url + 'all');
+        let data = await this.getData(url + 'all');
         if (data.length !== 0) {
             newCommunity.Cities = data.map(itm => new City(itm.id, itm.Name, itm.Latitude, itm.Longitude, itm.Population))
         }
